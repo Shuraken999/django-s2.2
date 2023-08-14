f9d731750a63dc0db8c1ec49dc573f0caa89e804
Shuraken999 <Shuraken999@mail.ru>
2023-08-14 21:33:13 +0400

    Добавил ДЗ Gjango 3.2 stocks_products

---
+++ stocks_products/logistic/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+
+from logistic.models import Product, StockProduct, Stock
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['address', 'positions']
+
+    # настройте сериализатор для склада
+
+    def create(self, validated_data):
+        positions = validated_data.pop('positions')
+        stock = super().create(validated_data)
+        for position in positions:
+            StockProduct.objects.update_or_create(stock=stock, **position)
+        print(stock)
+        return stock
+
+    def update(self, instance, validated_data):
+        positions = validated_data.pop('positions')
+        stock = super().update(instance, validated_data)
+        for item in positions:
+            StockProduct.objects.update_or_create(
+                stock=stock,
+                product=item.get('product'),
+                defaults={'price': item.get('price'), 'quantity': item.get('quantity')})
+        return stock
